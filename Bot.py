de7f02de8a29f58b2b48c1efe1c3b02da31cd65d
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-06-22 02:46:23 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -21,9 +21,9 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("APP_ID", ""))
-api_hash = os.environ.get("API_HASH", "")
-bot_token = os.environ.get("TOKEN", "")
+api_id = int(os.environ.get("APP_ID", "24267726"))
+api_hash = os.environ.get("API_HASH", "7500ba8248548cc3864bd033668f9a9a")
+bot_token = os.environ.get("TOKEN", "7094850446:AAHeGjIa_MB_8ljWjhHmJb2yAOnI9YUZxjg")
 client = TelegramClient("client", api_id, api_hash).start(bot_token=bot_token)
 spam_chats = []
 
